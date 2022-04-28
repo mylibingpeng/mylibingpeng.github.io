126f5479f2d622d8bd157d67280532fa57820ce3
libingpeng <3072506724@qq.com>
2022-04-29 04:10:53 +0800

    第一次提交

---
+++ Vue.js
@@ -0,0 +1,17 @@
+let vm = new Vue({
+    el:"#one_day",
+    data:{
+        one_Day:[
+            { titel:"悠哉日常大王第三季",href:"第12集" },
+            { titel:"女学。～圣女斯克威尔学院",href:"第32集" },
+            { titel:"进击的巨人第四季",href:"第43集" },
+            { titel:"IDOLY PRIDE",href:"第22集" },
+            { titel:"银魂",href:"第29集" },
+            { titel:"赛马娘第二季",href:"第97集" },
+            { titel:"好比是最终迷宫前的少年到新手村生活一般",href:"第17集" }
+        ]
+    },
+    methods:{
+
+    }
+})
